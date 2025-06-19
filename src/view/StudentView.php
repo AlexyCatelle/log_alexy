c3667aa9d9e4cac9249054b9fdc99f7ea9089ef9
AlexyCatelle <alexy.catelle@oclock.school>
2025-06-19 10:06:13 +0200

    update partie 3

--- src/view/StudentView.php
+++ src/view/StudentView.php
@@ -12,3 +12,17 @@ $studentService = new StudentService($studentRepo, $logRepo);
 $students = $studentService->getStudents();
 
 include 'src/view/StudentView.php';
+
+?>
+
+<h2>Formulaire Étudiant</h2>
+<form method="post">
+    <input type="hidden" name="action" value="save">
+    <label>ID (pour modification) : <input type="number" name="id"></label><br>
+    <label>Nom : <input type="text" name="nom" required></label><br>
+    <label>Prénom : <input type="text" name="prenom" required></label><br>
+    <label>Date de naissance : <input type="date" name="date_naissance" required></label><br>
+    <label>Email : <input type="email" name="email" required></label><br>
+    <button type="submit" name="submit" value="add">Ajouter</button>
+    <button type="submit" name="submit" value="update">Modifier</button>
+</form>
